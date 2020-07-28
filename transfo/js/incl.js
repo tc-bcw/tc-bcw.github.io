553b70cf5104d7b7eb4e7ff478efdb2f436749d8
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-07-28 11:02:07 -0700

    Update incl.js

--- transfo/js/incl.js
+++ transfo/js/incl.js
@@ -14,9 +14,9 @@ function includeHTML() {
           if (this.status == 200) {
             lang = elmnt.getAttribute("lang");
             if (lang) {
-              console.log("ok?");
-              console.log(this.responseText.search("id=\"lang-link\" href=\"#\""));
+              console.log("pls work");
               var newText = this.responseText;
+              console.log(newText.search("id=\"lang-link\" href=\"#\""));
               var newText2 = newText.replace("id=\"lang-link\" href=\"#\"","id=\"lang-link\" href=\"" + lang + "\"");
               elmnt.innerHTML = newText2;
             } else {
