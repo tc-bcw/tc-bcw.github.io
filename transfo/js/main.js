b9356be5dd78bde55283b947d4dc03d55e7b3e6e
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-07-29 10:30:11 -0700

    Stop modal script

--- transfo/js/main.js
+++ transfo/js/main.js
@@ -13,3 +13,15 @@ $(function () {
         $('#carouselIndicators').carousel('pause');
     });
 });
+
+var stopVideo = function ( element ) {
+    var iframe = element.querySelector( 'iframe');
+    var video = element.querySelector( 'video' );
+    if ( iframe !== null ) {
+        var iframeSrc = iframe.src;
+        iframe.src = iframeSrc;
+    }
+    if ( video !== null ) {
+        video.pause();
+    }
+};
