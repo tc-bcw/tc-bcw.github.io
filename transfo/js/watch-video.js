fd8ca41ec1c88605a4bda917a6764dcdc1257408
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-08-10 14:40:04 -0400

    Update watch-video.js

--- transfo/js/watch-video.js
+++ transfo/js/watch-video.js
@@ -1,8 +1,10 @@
-var queryString = window.location.search;
-console.log(queryString);
-var urlParams = new URLSearchParams(queryString);
-var videoID = urlParams.get('v')
-if (videoID) {
-    console.log("We're in the if statement.");
-    document.getElementById("pageContent").innerHTML = "<p>This worked ok</p>";
+$(document).ready(function() {
+    var queryString = window.location.search;
+    console.log(queryString);
+    var urlParams = new URLSearchParams(queryString);
+    var videoID = urlParams.get('v')
+    if (videoID) {
+        console.log("We're in the if statement.");
+        document.getElementById("pageContent").innerHTML = "<p>This worked ok</p>";
+    }
 }
