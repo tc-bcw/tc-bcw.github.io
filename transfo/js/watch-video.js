7b89ac7ff3b0aafc054be1195058e2f063805475
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-08-10 15:17:51 -0400

    Update watch-video.js

--- transfo/js/watch-video.js
+++ transfo/js/watch-video.js
@@ -5,6 +5,8 @@ window.addEventListener('load', (event) => {
     var videoID = urlParams.get('v');
     if (videoID) {
         console.log("We're in the if statement.");
-        document.getElementById("video-content").innerHTML = "<iframe id=\"video-embed\" width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/"+videoID+"?rel=0\" frameborder=\"0\" allowfullscreen></iframe>";
+        var vidContent = document.getElementById("video-content");
+        vidContent.innerHTML = "<iframe id=\"video-embed\" width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/"+videoID+"?rel=0\" frameborder=\"0\" allowfullscreen></iframe>";
+        vidContent.scrollIntoView();
     }
 });
