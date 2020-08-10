0590b59d39c41bdf532e37b79a79b6b57f72ad9f
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-08-10 14:42:11 -0400

    Update watch-video.js

--- transfo/js/watch-video.js
+++ transfo/js/watch-video.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var queryString = window.location.search;
     console.log(queryString);
     var urlParams = new URLSearchParams(queryString);
-    var videoID = urlParams.get('v')
+    var videoID = urlParams.get('v');
     if (videoID) {
         console.log("We're in the if statement.");
         document.getElementById("pageContent").innerHTML = "<p>This worked ok</p>";
