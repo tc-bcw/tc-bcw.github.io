e72d3ead1840964c95bc7d20d7859a3a08a23b57
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-08-12 12:12:31 -0400

    Update main.js

--- transfo/js/main.js
+++ transfo/js/main.js
@@ -19,7 +19,7 @@ var stopVideo = function ( element ) {
     frame.innerHTML = ' <div class="loader"></div> ';
 };
 
-// var vidModalFill = function ( vidId ) {
-//     var frame = document.getElementById('modal-body');
-//     frame.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + vidId + '" frameborder="0" allowfullscreen></iframe>"';
-// }
+var vidModalFill = function ( vidId ) {
+    var frame = document.getElementById('modal-body');
+    frame.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + vidId + '" frameborder="0" allowfullscreen></iframe>"';
+}
