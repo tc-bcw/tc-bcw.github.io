ef06b0dd099467fc3f018f595a89c7dd6e79b333
tc-bcw <64495159+tc-bcw@users.noreply.github.com>
2020-08-12 16:12:56 -0400

    Update main.js

--- transfo/js/main.js
+++ transfo/js/main.js
@@ -28,5 +28,5 @@ var stopVideo = function () {
 
 
 var vidModalFill = function ( vidId ) {
-    $('modal-body').html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + vidId + '?rel=0" frameborder="0" allowfullscreen></iframe>"');
+    $('#modal-body').html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + vidId + '?rel=0" frameborder="0" allowfullscreen></iframe>"');
 };
